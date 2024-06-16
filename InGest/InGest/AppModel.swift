6eae5441bda039ff46997f66131095e8d262ec38
Adrian Eves <adrianevesdev@gmail.com>
2024-06-16 14:46:58 -0500

    InGest: initial commit

---
+++ InGest/InGest/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  InGest
+//
+//  Created by Adrian Eves on 6/16/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ InGest/InGest/InGestApp.swift
@@ -0,0 +1,33 @@
+//
+//  InGestApp.swift
+//  InGest
+//
+//  Created by Adrian Eves on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct InGestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
